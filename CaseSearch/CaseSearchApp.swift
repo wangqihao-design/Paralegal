a16441fb7f3b68f192a34fad69f1984f8d7c8120
wangqihao-design <wangqihaozuishuai@icloud.com>
2022-07-16 12:48:00 -0700

    Initial Commit

---
+++ CaseSearch/CaseSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseSearchApp.swift
+//  CaseSearch
+//
+//  Created by 麒昊王 on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct CaseSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaseSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CaseSearch
+//
+//  Created by 麒昊王 on 7/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
